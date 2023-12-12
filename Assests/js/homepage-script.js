3113aa57e62e0ffa2873ecec8fc3a0709288ab37
Jaspreet Singh <34242488+jaspreet97977@users.noreply.github.com>
2023-12-12 18:30:18 -0500

    Homepage Draft

---
+++ Assests/js/homepage-script.js
@@ -0,0 +1,22 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex = (slideIndex >= slides.length) ? 1 : slideIndex + 1;
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}

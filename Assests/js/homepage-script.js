0602228aa01e5237e82abaafd26fb2ff63cde3c0
Jaspreet Singh <34242488+jaspreet97977@users.noreply.github.com>
2023-12-14 14:42:30 -0500

    Payment Page

--- Assests/js/homepage-script.js
+++ Assests/js/homepage-script.js
@@ -20,3 +20,26 @@ function showSlides() {
 
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+// validate payment page forms
+function validateForm() {
+    // Simple validation logic (you can enhance this based on your requirements)
+    var cardNumber = document.getElementById("cardNumber").value;
+    var expiryDate = document.getElementById("expiryDate").value;
+    var cvv = document.getElementById("cvv").value;
+    var cardHolderName = document.getElementById("cardHolderName").value;
+
+    if (!cardNumber || !expiryDate || !cvv || !cardHolderName) {
+        alert("Please fill in all required fields.");
+        return false;
+    }
+
+    // Simulate successful payment
+    alert("Payment successful!");
+
+    // Redirect to index.html (you can replace this with your actual redirect logic)
+    window.location.href = "index.html";
+
+    return false; // Prevent form submission (to keep the alert visible for this example)
+}
